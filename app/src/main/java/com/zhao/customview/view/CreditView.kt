c6b7c5dd93e183bb4f9531e983a30968b607b98d
gaowenzhao <510368603@qq.com>
2019-06-04 14:50:35 +0800

    自定义仿信用积分控件

--- app/src/main/java/com/zhao/customview/view/CreditView.kt
+++ app/src/main/java/com/zhao/customview/view/CreditView.kt
@@ -7,8 +7,6 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AccelerateInterpolator
-import android.graphics.SweepGradient
-
 
 
 class CreditView : View {
@@ -21,10 +19,14 @@ class CreditView : View {
     private var skPaint = Paint() //小刻度
     private var lkPaint = Paint() //大刻度
     private var mBitmapPaint = Paint()//小圆点画笔
+    private var sTextPaint = Paint() //刻度上的字体
     private val wStrokeWidth = 10f
     private val nStrokeWidth = 30f
     private val cirLocation by lazy {  FloatArray(2).apply { set(0,paddingLeft.toFloat());set(1,radus) } }
-    var progress = 0f
+    private val levelStrs  = arrayOf("较差","中等","良好","优秀","极好","完美")
+    private val levels  = arrayOf("350","550","600","650","700","950","1150")
+//    private
+    private var progress = 0f
     private var setpro = 0f
     private val valueAnimator by lazy { createAnimator() }
     constructor(context: Context?) : super(context)
@@ -62,6 +64,13 @@ class CreditView : View {
         lkPaint.color = Color.WHITE
         lkPaint.strokeWidth = 2f
 
+        sTextPaint = Paint()
+        sTextPaint.isAntiAlias = true
+        sTextPaint.color = Color.WHITE
+        sTextPaint.textSize = 20f
+        sTextPaint.alpha = 100
+        sTextPaint.textAlign = Paint.Align.CENTER
+
         mBitmapPaint = Paint()
         mBitmapPaint.isAntiAlias = true
         mBitmapPaint.color = Color.WHITE
@@ -72,8 +81,6 @@ class CreditView : View {
         //画外环
         val wRectf = RectF(paddingLeft.toFloat(),paddingTop.toFloat(),(width-paddingRight).toFloat(),(height-paddingBottom).toFloat())
         canvas.drawArc(wRectf,180f,180f,false,wPaint)
-
-
         //画内环
         val left = paddingLeft+distance
         val top = paddingTop+distance
@@ -82,31 +89,40 @@ class CreditView : View {
         val nRectf = RectF(left,top,right,bottom)
         canvas.drawArc(nRectf,180f,180f,false,nPaint)
         //画小刻度
-        canvas.save()
-        canvas.rotate(-90f,radus,radus)
         val startX = radus
         val startY = paddingTop+distance-nStrokeWidth/2
         val stopX = radus
         val stopY = startY+nStrokeWidth
-        val stopYs = stopY+5f
+        val stopYs = stopY+dp2px(3f)
+        val baseLineY = stopYs+ dp2px(10f)
+        canvas.save()
+        canvas.rotate(-90f,radus,radus)
         for(i in 1..36){
             canvas.drawLine(startX,startY,stopX,stopY,skPaint)
             canvas.rotate(5f,radus,radus)
         }
         canvas.restore() //回滚save之前得状态
-        canvas.save()
         //画大刻度
+        canvas.save()
         canvas.rotate(-90f,radus,radus)
-        for(i in 1..10){
+        for(i in 0 until levels.size){
             canvas.drawLine(startX,startY,stopX,stopYs,lkPaint)
-            canvas.rotate(20f,radus,radus)
+            canvas.drawText(levels[i],stopX,baseLineY,sTextPaint)
+            canvas.rotate(180f/(levels.size-1),radus,radus)
+        }
+        canvas.restore() //回滚save之前得状态
+        //画信用等级
+        canvas.save() //相当于保存之前画布的界面，然后在新的画布操作
+        canvas.rotate(-75f,radus,radus)
+        for(i in 0 until levelStrs.size){
+            canvas.drawText(levelStrs[i],stopX,baseLineY,sTextPaint)
+            canvas.rotate(180f/levelStrs.size,radus,radus)
         }
         canvas.restore() //回滚save之前得状态
         //把渐变设置到笔刷
         proPaint.shader = LinearGradient(paddingLeft.toFloat(),radus,cirLocation[0],cirLocation[1],Color.RED,Color.BLUE,Shader.TileMode.CLAMP)
         //画外环进度
         canvas.drawArc(wRectf,180f,progress,false,proPaint)
-
        //画外圆环进度的小圆点
         val path = Path()
         path.addArc(wRectf,180f,progress)
